3ba7cc2808259e5d1288b24df3811118e110e8e3
Banyapon Poolsawas <daydevthailand@gmail.com>
2024-02-06 13:15:57 +0700

    Fixed Form Validation

--- js/app.js
+++ js/app.js
@@ -2,14 +2,22 @@ var database = firebase.database();
 document.getElementById("add-data").onclick = function (e) {
     var fullname = document.getElementById('fullname').value;
     var email = document.getElementById('email').value;
-    var rootRef = firebase.database().ref();
-    var storesRef = rootRef.child('jbc/registration/');
-    var newStoreRef = storesRef.push();
-    newStoreRef.set({
-        fullname: fullname,
-        email: email,
-    }).then(data => {
-      console.log("OK");
-      document.getElementById("alertBlock").style.display = "block";
-    });
+    if (fullname == "" || email == "") {
+        document.getElementById("failed").style.display = "block";
+    } else {
+        var rootRef = firebase.database().ref();
+        var storesRef = rootRef.child('jbc/registration/');
+        var newStoreRef = storesRef.push();
+        newStoreRef.set({
+            fullname: fullname,
+            email: email,
+        }).then(data => {
+            console.log("OK");
+            document.getElementById("alertBlock").style.display = "block";
+            document.getElementById("failed").style.display = "none";
+            document.getElementById("add-data").style.display = "none";
+            document.getElementById("fullname").style.display = "none";
+            document.getElementById("email").style.display = "none";
+        });
+    }
 }
